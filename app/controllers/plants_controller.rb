bc0fe756e75161adbb7582aa80fd0ca5b4bb8b99
Micaela Frank <micaelalfrank@gmail.com>
2022-06-28 14:44:10 -0400

    commit tree 219edc3173f28923bec4764c67ba91f2ee2dc0ba

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -12,13 +13,36 @@ class PlantsController < ApplicationController
     render json: plant
   end
 
+  #PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(is_in_stock: false)
+    render json: plant
+  end
+
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
 
+  #DELETE /plant/id
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
